94dac3861b42c128199bcefe83c9078dfa6669e9
DDevil_ <brunorbsf@gmail.com>
2018-01-19 13:32:00 -0300

    - Added wiki items

---
+++ Assets/Shiroi/Cutscenes/Examples/MyToken.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using Shiroi.Cutscenes.Tokens;
+using UnityEngine;
+
+namespace Shiroi.Cutscenes.Examples {
+    public class MyToken : IToken {
+        public ExposedReference<ParticleSystem> System;
+        public Vector3 Position;
+
+        public IEnumerator Execute(CutscenePlayer player) {
+            var systemInScene = System.Resolve(player);
+            Object.Instantiate(systemInScene, Position, Quaternion.identity);
+            yield break;
+        }
+    }
+}
