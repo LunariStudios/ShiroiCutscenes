2041e303af960f8b7f6c8d00a4977583bf73e0a5
DDevil_ <brunorbsf@gmail.com>
2018-01-22 19:37:29 -0300

    - Starting to work on serialization

---
+++ Assets/Shiroi/Cutscenes/Serialization/Serializer.cs
@@ -0,0 +1,14 @@
+﻿namespace Shiroi.Cutscenes.Serialization {
+    public abstract class Serializer {
+        public abstract void Serialize(object value, string name, ISerializedObject destination);
+        public abstract object Deserialize(string key, ISerializedObject obj);
+    }
+
+    public abstract class Serializer<T> : Serializer {
+        public override void Serialize(object value, string name, ISerializedObject destination) {
+            Serialize((T) value, name, destination);
+        }
+
+        public abstract void Serialize(T value, string name, ISerializedObject destination);
+    }
+}
