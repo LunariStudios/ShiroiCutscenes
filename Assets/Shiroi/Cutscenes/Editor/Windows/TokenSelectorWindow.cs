d47ee88c655b720ce061c6a4b52a03a6deebbecf
DDevil_ <brunorbsf@gmail.com>
2018-02-03 13:34:51 -0300

    - Added token filtering in selector window

--- Assets/Shiroi/Cutscenes/Editor/Windows/TokenSelectorWindow.cs
+++ Assets/Shiroi/Cutscenes/Editor/Windows/TokenSelectorWindow.cs
@@ -1,32 +1,58 @@
-﻿using UnityEditor;
+﻿using System;
+using Shiroi.Cutscenes.Editor.Util;
+using UnityEditor;
 using UnityEngine;
 
 namespace Shiroi.Cutscenes.Editor.Windows {
     public class TokenSelectorWindow : PopupWindowContent {
+        // Label
+        // Filter
+        public const int BuiltInLines = 2;
+
+        public const string FilterLabel = "Filter";
+        public const float WindowWidth = 300;
 
         public static Vector2 Size {
             get {
-                return new Vector2(200, (TokenLoader.KnownTokenTypes.Count + 1) * EditorGUIUtility.singleLineHeight);
+                return new Vector2(WindowWidth,
+                    (TokenLoader.KnownTokenTypes.Count + BuiltInLines) * ShiroiStyles.SingleLineHeight);
             }
         }
 
         public TokenSelectorWindow(CutsceneEditor currentEditor) {
             CurrentEditor = currentEditor;
         }
-        public CutsceneEditor CurrentEditor { get; private set; }
+
+        public CutsceneEditor CurrentEditor {
+            get;
+            private set;
+        }
 
         public override Vector2 GetWindowSize() {
             return Size;
         }
 
+        private string filter = string.Empty;
+
         public override void OnGUI(Rect rect) {
             EditorGUI.LabelField(CutsceneEditor.GetRect(rect, 0), "Select a token to add");
-            for (var i = 0; i < TokenLoader.KnownTokenTypes.Count; i++) {
-                var type = TokenLoader.KnownTokenTypes[i];
+            EditorGUI.BeginChangeCheck();
+            filter = EditorGUI.TextField(rect.GetLine(1), FilterLabel, filter);
+            if (EditorGUI.EndChangeCheck()) {
+                editorWindow.Repaint();
+                return;
+            }
+            var i = 0;
+            foreach (var type in TokenLoader.KnownTokenTypes) {
+                if (!string.IsNullOrEmpty(filter) &&
+                    !type.Name.StartsWith(filter, StringComparison.InvariantCultureIgnoreCase)) {
+                    continue;
+                }
                 GUI.color = MappedToken.For(type).Color;
-                if (GUI.Button(CutsceneEditor.GetRect(rect, i + 1), type.Name)) {
+                if (GUI.Button(CutsceneEditor.GetRect(rect, i + BuiltInLines), type.Name)) {
                     CurrentEditor.AddToken(type);
                 }
+                i++;
             }
         }
     }
