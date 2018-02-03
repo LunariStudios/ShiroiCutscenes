8e146b3b8eb3037c0b7f8c2ad32cc9cb1df07119
DDevil_ <brunorbsf@gmail.com>
2018-02-03 11:28:59 -0300

    - Fixed a bug in Reference checkers that would throw false positive errors

--- Assets/Shiroi/Cutscenes/Editor/Errors/DefaultErrorCheckers.cs
+++ Assets/Shiroi/Cutscenes/Editor/Errors/DefaultErrorCheckers.cs
@@ -6,7 +6,8 @@ using Shiroi.Cutscenes.Util;
 
 namespace Shiroi.Cutscenes.Editor.Errors {
     public class MissingFutureChecker : ErrorChecker {
-        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token, object value, int fieldIndex, FieldInfo info) {
+        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token,
+            object value, int fieldIndex, FieldInfo info) {
             var reference = value as FutureReference;
             if (reference == null) {
                 return;
@@ -22,23 +23,36 @@ namespace Shiroi.Cutscenes.Editor.Errors {
 
 
     public class MissingReferenceChecker : ErrorChecker {
-        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token, object value, int fieldIndex, FieldInfo info) {
+        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token,
+            object value, int fieldIndex, FieldInfo info) {
             var reference = value as Reference;
             var player = editor.Player;
             if (reference == null || player == null) {
                 return;
             }
             var id = reference.Id;
-            if (reference.Resolve(player) != null) {
+            if (Resolve(reference, editor.Cutscene, player)) {
                 return;
             }
             var msg = string.Format("Couldn't resolve reference of id {0} in field {1}.", id, info.Name);
             manager.NotifyError(tokenIndex, fieldIndex, ErrorLevel.High, msg, "You probably didn't assign it.");
         }
+
+        private bool Resolve(Reference reference, Cutscene cutscene, CutscenePlayer player) {
+            switch (reference.Type) {
+                case Reference.ReferenceType.Future:
+                    return cutscene.FutureManager.GetFuture(reference.Id) != null;
+                case Reference.ReferenceType.Exposed:
+                    return reference.Resolve(player) != null;
+                default:
+                    return false;
+            }
+        }
     }
 
     public class NullChecker : ErrorChecker {
-        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token, object value, int fieldIndex, FieldInfo info) {
+        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token,
+            object value, int fieldIndex, FieldInfo info) {
             if (Attribute.GetCustomAttribute(info, typeof(NullSupportedAttribute)) != null) {
                 return;
             }
@@ -53,7 +67,8 @@ namespace Shiroi.Cutscenes.Editor.Errors {
     }
 
     public class EmptyStringChecker : ErrorChecker {
-        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token, object value, int fieldIndex, FieldInfo info) {
+        public override void Check(CutsceneEditor editor, ErrorManager manager, int tokenIndex, IToken token,
+            object value, int fieldIndex, FieldInfo info) {
             if (Attribute.GetCustomAttribute(info, typeof(EmptyStringSupportedAttribute)) != null) {
                 return;
             }
