beb3c1b122f621cf498e92a2246a5b20252d0a69
DDevilISL <brunorbsf@gmail.com>
2018-05-16 16:25:50 -0300

    - Added SetGameObjectActiveToken.cs - Added PostActions to CutsceneTriggers

---
+++ ShiroiCutscenes-Runtime/Tokens/SetGameObjectActiveToken.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Shiroi.Cutscenes.Tokens {
+    public class SetGameObjectActiveToken : Token {
+        public ExposedReference<GameObject> Target;
+        public bool Active;
+
+        public override IEnumerator Execute(CutscenePlayer player) {
+            var go = Target.Resolve(player);
+            if (go != null) {
+                go.SetActive(Active);
+            }
+
+            yield break;
+        }
+    }
+}
--- ShiroiCutscenes-Runtime/Triggers/CutsceneTrigger.cs
+++ ShiroiCutscenes-Runtime/Triggers/CutsceneTrigger.cs
@@ -1,15 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Shiroi.Cutscenes.Triggers {
     public abstract class CutsceneTrigger : MonoBehaviour {
+        public enum PostPlayAction {
+            None,
+            Disable,
+            DestroyGameObject,
+            DestroySelf
+        }
+
         public Cutscene Cutscene;
         public CutscenePlayer Player;
+        public PostPlayAction PostAction = PostPlayAction.DestroyGameObject;
+        private bool played;
+
         protected void Trigger() {
             if (!Player) {
                 Debug.LogWarning("[ShiroiCutscenes] Couldn't find an active instance of CutscenePlayer!");
                 return;
             }
+
+            if (PostAction != PostPlayAction.None && played) {
+                return;
+            }
+
+            played = true;
             Player.Play(Cutscene);
+            switch (PostAction) {
+                case PostPlayAction.None:
+                    break;
+                case PostPlayAction.Disable:
+                    enabled = false;
+                    break;
+                case PostPlayAction.DestroyGameObject:
+                    Destroy(gameObject);
+                    break;
+                case PostPlayAction.DestroySelf:
+                    Destroy(this);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
