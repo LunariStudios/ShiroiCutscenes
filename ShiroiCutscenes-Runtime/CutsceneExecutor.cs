bfbf99bd9611d6bd6ea3b76565933328e9dc5baf
DDevilISL <brunorbsf@gmail.com>
2018-06-20 23:48:06 -0300

    - Fixed a couple bugs in CutsceneExecutor.cs

--- ShiroiCutscenes-Runtime/CutsceneExecutor.cs
+++ ShiroiCutscenes-Runtime/CutsceneExecutor.cs
@@ -3,18 +3,24 @@
 namespace Shiroi.Cutscenes {
     public class CutsceneExecutor {
         private bool shouldSkip;
+        private readonly Cutscene cutscene;
+        private readonly CutscenePlayer player;
 
         public Cutscene Cutscene {
-            get;
+            get {
+                return cutscene;
+            }
         }
 
         public CutscenePlayer Player {
-            get;
+            get {
+                return player;
+            }
         }
 
         public CutsceneExecutor(Cutscene cutscene, CutscenePlayer player) {
-            Cutscene = cutscene;
-            Player = player;
+            this.cutscene = cutscene;
+            this.player = player;
         }
 
         public IEnumerator Execute() {
@@ -28,7 +34,12 @@ namespace Shiroi.Cutscenes {
         }
 
         private static void Consume(IEnumerator execute) {
-            while (execute.MoveNext()) { }
+            while (execute.MoveNext()) {
+                var current = execute.Current as IEnumerator;
+                if (current != null) {
+                    Consume(current);
+                }
+            }
         }
 
 
